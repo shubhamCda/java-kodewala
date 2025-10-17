13fb48231ce709636e42ef414fe8274b1fcf7ee0
shubhamCda <shubhambodalkar281@gmail.com>
2025-10-17 12:27:52 +0530

    Add Exception handling nullpointer

--- ExceptionHandling/src/com/kodewala/exceptionhandling/ExceptionHandlingEx.java
+++ ExceptionHandling/src/com/kodewala/exceptionhandling/ExceptionHandlingEx.java
@@ -6,13 +6,24 @@ public class ExceptionHandlingEx {
 		// TODO Auto-generated method stub
 
 		String s = "shubam";
-		String s2 = null;
+//		int s0 = Integer.parseInt(s); // java.lang.ArithmeticException
 
 //		System.out.println(s.length());
-		System.out.println(s2.length()); // java.lang.ArithmeticException
+		System.out.println("program START's here....");
+		try {
+			String s2 = null;
 
-		int exp = 10 / 0;
-		System.out.println(exp); //java.lang.ArithmeticException:
+			System.out.println(s2.length()); // java.lang.ArithmeticException
+		} catch (NullPointerException e) {
+			// TODO: handle exception
+			System.out.println("String is null....");
+//			e.printStackTrace(); // tells the line no. on which exception occurs.
+		}
+
+		System.out.println("Program END's here....");
+
+//		int exp = 10 / 0;
+//		System.out.println(exp); //java.lang.ArithmeticException:
 
 	}
 
