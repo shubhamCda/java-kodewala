d4b6464c7119556f47479ec09005af2a2184d6b3
shubhamCda <shubhambodalkar281@gmail.com>
2025-10-17 12:36:23 +0530

    Add seperate catch block for every exception

--- ExceptionHandling/src/com/kodewala/exceptionhandling/ExceptionHandlingEx.java
+++ ExceptionHandling/src/com/kodewala/exceptionhandling/ExceptionHandlingEx.java
@@ -5,12 +5,12 @@ public class ExceptionHandlingEx {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String s = "shubam";
 //		int s0 = Integer.parseInt(s); // java.lang.ArithmeticException
 
 //		System.out.println(s.length());
 		System.out.println("program START's here....");
 		try {
+			String s = args[2];
 			String s2 = null;
 
 			System.out.println(s2.length()); // java.lang.ArithmeticException
@@ -19,7 +19,11 @@ public class ExceptionHandlingEx {
 			System.out.println("String is null....");
 //			e.printStackTrace(); // tells the line no. on which exception occurs.
 		}
-
+		catch (ArrayIndexOutOfBoundsException e) {
+			// TODO: handle exception
+			System.out.println("Array INDEX is not present....");
+			e.printStackTrace();
+		}
 		System.out.println("Program END's here....");
 
 //		int exp = 10 / 0;
