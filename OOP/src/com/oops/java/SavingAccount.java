e7ed600d5187ad6ccbd79c963fe5d4dbef76e9e3
shubhamCda <shubhambodalkar281@gmail.com>
2025-10-20 07:26:32 +0530

    Add OOP in java learning

---
+++ OOP/src/com/oops/java/SavingAccount.java
@@ -0,0 +1,26 @@
+package com.oops.java;
+
+public class SavingAccount {
+	private String name;
+	private String accountNumber;
+	private int balance;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+}
