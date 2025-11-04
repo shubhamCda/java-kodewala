8580724e1d17dea3923a734702e10ea25053f5b5
shubhamCda <shubhambodalkar281@gmail.com>
2025-11-04 10:11:22 +0530

    Add Default variable values concept

---
+++ 27-oct/day-6/Student.java
@@ -0,0 +1,24 @@
+class Student
+{
+	static String name;
+	static int classroom;
+	static boolean present;
+	static long marks;
+	static float percentage;
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Name: "+name);
+		System.out.println("Class: "+ classroom);
+		System.out.println("Available: "+present);
+		System.out.println("Final marks scored: "+marks);
+		System.out.println("Toatal percentage: "+percentage);
+		
+		int age; //local variable will not get default value
+		System.out.println("age: "+age); //variable age might not have been initialized
+               
+                                           
+		
+	}
+}
+		
