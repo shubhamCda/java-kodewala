9aa3adca900d56b44855d20244f0a8b2a4544602
shubhamCda <shubhambodalkar281@gmail.com>
2025-11-05 10:46:22 +0530

    Add access modifier concept

---
+++ 27-oct/day-7/Account.java
@@ -0,0 +1,5 @@
+class Account
+{
+	static int amount = 2000; //default access modifier
+}
+
---
+++ 27-oct/day-7/Banking.java
@@ -0,0 +1,14 @@
+class Banking
+{
+	private static int loanAmount = 1000;
+}
+
+class Payment
+{
+	//int value = Banking.loanAmount;
+	
+	public static void main(String[] args)
+	{
+		System.out.println(Account.amount);
+	}
+}
